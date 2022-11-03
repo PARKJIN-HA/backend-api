20d098117074f7fa0e68bd799e80663e3dbd6d06
Pjinha <qq0201@naver.com>
2022-11-03 13:54:28 +0900

    Change Uvicorn host

--- main.py
+++ main.py
@@ -148,4 +148,4 @@ async def get_schedule_by_member(user_id: str, db: Session = Depends(get_db)):
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="localhost", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
